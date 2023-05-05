dbaff1c2ea407803a4a0050eebed11c4a32bcf8e
kripy-kd <97969038+kripy-kd@users.noreply.github.com>
2023-05-05 20:02:45 +0530

    Add files via upload

---
+++ nfcn/dist/script.js
@@ -0,0 +1,68 @@
+/*use JS here to enhance preloaded images and buttons. This includes popup buttons. Refer to Hover Animation For Image Descriptions Pen */ 
+
+
+
+/**(function() {
+  $('.square').hover(
+    function() {
+      $(this).children('.hover-image').stop().show('fade', 100).animate({
+        marginTop: '-100px',
+        height: '200px',
+        opacity: 1.0
+      }, 290, 'swing');
+      
+      $(this).children('.hover-text').stop().show('fade', 100).animate({
+        marginTop: '100px',
+        height: '200px',
+        opacity: 1.0
+      }, 290, 'swing');
+      
+      $(this).children('.hover-arrow').stop().show('fade', 100).animate({
+        opacity: 1.0
+      }, 290, 'swing');
+    },
+    function() {
+      $(this).children('.hover-image').stop().animate({
+        marginTop: '100px',
+        height: '0px',
+        opacity: 0
+      }, 290, 'swing', function(){
+        $(this).hide('fade', 100);
+      });
+      
+      $(this).children('.hover-text').stop().animate({
+        marginTop: '100px',
+        height: '0px',
+        opacity: 0
+      }, 290, 'swing', function(){
+        $(this).hide('fade', 100);
+      });
+      
+      $(this).children('.hover-arrow').stop().animate({
+        opacity: 0
+      }, 290, 'swing', function() {
+        $(this).hide('fade', 100);
+      });
+   });
+});**/
+
+
+
+
+
+
+
+
+
+
+
+
+/**function bigImg(x) {
+  x.style.height = "200px";
+  x.style.width = "200px";
+}
+
+function normalImg(x) {
+  x.style.height = "100px";
+  x.style.width = "100px";
+} **/
